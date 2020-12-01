c05c5d14e7e67e7ae197ff4f37c02b6c65469f9e
SpiritKing <mrepetschnig@tsn.at>
2020-12-01 20:58:16 +0100

    Datenbanken funktionieren jetzt

--- AktienProjekt_KS_4aHWII/Main.java
+++ AktienProjekt_KS_4aHWII/Main.java
@@ -6,9 +6,23 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException, JSONException {
+String datum = "";
+String closeValue = "";
+        Methode m = new Methode();
+String unternehmen = m.unternehmenName();
+
+m.connectToAPI(unternehmen);
+        m.connectDB(m.urlDB);
+        m.createNewDatabase(m.urlDB );
+        m.createTableofDB(m.urlDB, unternehmen);
+ for(int i = 0; i< Methode.dates.size(); i++){
+     datum = m.dates.get(i).toString();
+     closeValue = m.HMzwischenspeicher.get(m.dates.get(i));
+     m.insert(unternehmen,datum,closeValue);
+ }
+        m.selectDB(unternehmen);
 
 
-Methoden.connectToAPI(Methoden.unternehmenName());
 
     }
 }
